9657878ab26192582e7bfcafc9d3d664cfc78991
Saurabh Tripathi <saurabh.umesh.tripathi@gmail.com>
2023-07-06 18:04:28 +0530

    changed files app.jsx,main.jsx, index.html

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,10 @@
 // import viteLogo from '../public/vite.svg'
 
 import './App.css'
-
+import Info from './Info.jsx'
+import About from './About.jsx'
+import Interests from './Interests.jsx'
+import Footer from './Footer.jsx'
 function App() {
  
 
